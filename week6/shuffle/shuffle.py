f4a367e044564ffc3523d63f7189a299a56646f1
github_Jbhupp <jenni.hupp@bryanuniversity.edu>
2023-08-26 19:29:35 -0400

    commit

--- week6/shuffle/shuffle.py
+++ week6/shuffle/shuffle.py
@@ -20,3 +20,6 @@ print(shuffleList([7, 20, 26, 31, 40, 51, 55, 63, 74, 81]))
 
 end_time = time.time()
 print(f"the end time is: {end_time-start_time}")
+
+# I think the time complexity of this algorithm is O(1) since I am only iterating through the length of the list, 
+# creating a random index, taking the current index and random index, swapping them around, and this process only completes once. 
